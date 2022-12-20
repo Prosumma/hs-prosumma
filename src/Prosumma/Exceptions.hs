a17f27e7767e2956a0dfe1b719b57222f2569b22
Gregory Higley <greg@prosumma.com>
2022-12-20 01:30:57 -0500

    Exceptions

--- src/Prosumma/Exceptions.hs
+++ src/Prosumma/Exceptions.hs
@@ -3,23 +3,50 @@ module Prosumma.Exceptions (
   catchMatch,
   matchException,
   throwMatch,
-  whenException
+  throwWhen
 ) where
 
+import Control.Monad.Error.Class
 import Prosumma.Logging
 import RIO hiding (log)
 import Text.Printf
 
-type MatchException source destination = source -> Either destination source
+type MatchException original matched = original -> Either matched original
 
-matchException :: (Exception source, Exception destination) => MatchException source destination -> MatchException SomeException SomeException 
-matchException handler source = case fromException source of
-  Nothing -> return source
+-- | Attempts to match `SomeException` to the type of `original`.
+--
+-- If a match occurs, then `handler` is executed. The `handler` can further examine the exception to
+-- decide whether it is a match. If it is, it should throw that exception, i.e., return it as `Left`
+-- to short-circuit all matching. Otherwise, it should return the original as `Right` so that it can be passed
+-- to the next handler. Handlers are typically stitched together using `>=>`:
+--
+-- > matchSqlState :: ByteString -> ServerError -> MatchException SqlError ServerError
+-- > matchSqlState code = throwWhen $ \sqe -> sqlState sqe == code 
+-- >
+-- > matchNoApp :: MatchException SqlError ServerError 
+-- > matchNoApp = matchSqlState "NOAPP" err404 { errReasonPhrase = "The specified app does not exist." } 
+-- >
+-- > matchNoUsr :: MatchException SqlError ServerError
+-- > matchNoUsr = matchSqlState "NOUSR" err404 { errReasonPhrase = "The specified user does not exist." }  
+-- >
+-- > matchException (matchNoApp >=> matchNoUsr)
+matchException :: (Exception original, Exception matched) => MatchException original matched -> MatchException SomeException SomeException 
+matchException handler original = case fromException original of
+  Nothing -> return original
   Just e -> bimap toException toException $ handler e 
 
-whenException :: (source -> Bool) -> destination -> MatchException source destination 
-whenException cond destination source = if cond source then Left destination else Right source
+-- | A combinator useful for building `MatchException`s.
+-- 
+-- > matchSqlState :: ByteString -> ServerError -> MatchException SqlError ServerError
+-- > matchSqlState code = throwWhen $ \sqe -> sqlState sqe == code
+throwWhen :: (original -> Bool) -> matched -> MatchException original matched
+throwWhen cond matched original = if cond original then throwError matched else return original
 
+-- | Used by `catchMatch` to attempt to match an exception. 
+--
+-- `throwMatch` passes the original exception through the match chain, rethrowing the left-hand side if a match occurred
+-- or the right-hand side if no match occurred. A match chain typically consists of a series of `MatchException` instances
+-- joined by `>=>`.
 throwMatch :: (MonadThrow m, HasLogging m, MonadIO m) => MatchException SomeException SomeException -> SomeException -> m a
 throwMatch match e = do
   log $ printf "throwMatch: %s" (show e)
@@ -27,5 +54,15 @@ throwMatch match e = do
     Left e -> throwM e
     Right e -> throwM e
 
+-- | Attempts to perform `action`. If an exception is thrown, it uses `throwMatch` to attempt to match and rethrow
+-- the exception if needed.
+--
+-- This is typically used to match underlying errors -- often `SqlError` instances thrown by database operations -- onto
+-- `ServerError` instances required by Servant.
+--
+-- > defaultSqlToServerErrors :: MatchException SomeException SomeException
+-- > defaultSqlToServerErrors = matchException $ matchNoApp >=> matchNoUsr
+-- >
+-- > catchMatch defaultSqlToServerErrors getUser
 catchMatch :: (MonadUnliftIO m, MonadThrow m, HasLogging m) => MatchException SomeException SomeException -> m a -> m a
 catchMatch match action = catchAny action $ throwMatch match 
