c4205529a4a6d5339537530bba6939d106f8f7bc
Gregory Higley <greg@prosumma.com>
2022-12-20 10:11:55 -0500

    Prosumma.hs

--- src/Prosumma/Util.hs
+++ src/Prosumma/Util.hs
@@ -1,2 +1,13 @@
 module Prosumma.Util (
+  also,
+  fmapMaybeM
 ) where
+
+import RIO
+
+also :: Monad m => (a -> m b) -> a -> m a
+also f a = f a >> return a
+
+fmapMaybeM :: Monad m => (a -> m b) -> Maybe a -> m (Maybe b)
+fmapMaybeM _ Nothing = return Nothing
+fmapMaybeM f (Just x) = f x <&> Just
