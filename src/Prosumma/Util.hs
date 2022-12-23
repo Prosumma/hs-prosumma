fbe9093b805c1015df146b30fa5fa14a8ed2876c
Gregory Higley <greg@prosumma.com>
2022-12-23 13:32:06 -0500

    Pair operators

--- src/Prosumma/Util.hs
+++ src/Prosumma/Util.hs
@@ -1,6 +1,8 @@
 module Prosumma.Util (
   also,
-  fmapMaybeM
+  fmapMaybeM,
+  (<->),
+  (<#>)
 ) where
 
 import RIO
@@ -11,3 +13,19 @@ also f a = f a >> return a
 fmapMaybeM :: Monad m => (a -> m b) -> Maybe a -> m (Maybe b)
 fmapMaybeM _ Nothing = return Nothing
 fmapMaybeM f (Just x) = f x <&> Just
+
+-- | Shortcut to create a pair.
+(<->) :: a -> b -> (a, b)
+a <-> b = (a, b)
+
+infixl 8 <->
+
+-- | Shortcut to create a pair in a list
+-- 
+-- Useful for creating maps:
+--
+-- > Map.fromList $ "foo" <#> "bar" <> "bing" <#> "bang"
+(<#>) :: a -> b -> [(a, b)]
+a <#> b = [a <-> b] 
+
+infixl 7 <#>
