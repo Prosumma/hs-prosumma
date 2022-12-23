6de81494ded4ec18ff9c56f1504228c7db3df60a
Gregory Higley <greg@prosumma.com>
2022-12-23 14:10:54 -0500

    Util tests

---
+++ test/Spec/Util.hs
@@ -0,0 +1,23 @@
+{-# OPTIONS_GHC -Wno-type-defaults #-}
+module Spec.Util (testUtil) where
+
+import Prosumma.Util
+import RIO
+import RIO.Map (fromList)
+import Test.Hspec
+
+testUtil :: Spec
+testUtil = do 
+  describe "<->" $
+    it "stitches together a pair" $
+      2 <-> 3 `shouldBe` (2, 3)
+  describe "<#>" $
+    it "stitches together a pair into a list" $ do
+      let expected = fromList [(2, 3), (4, 5)]
+      let tested = fromList $ 2 <#> 3 <> 4 <#> 5
+      expected `shouldBe` tested
+  describe "<=>" $
+    it "stitches together a pair into a Map" $ do
+      let expected = fromList [(2, 3), (4, 5)]
+      let tested = 2 <=> 3 <> 4 <=> 5
+      expected `shouldBe` tested
