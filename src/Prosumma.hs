4e9831dd3352e125765751f13b20f122cf9a626a
Gregory Higley <greg@prosumma.com>
2022-12-26 14:36:06 -0500

    Textual

--- src/Prosumma.hs
+++ src/Prosumma.hs
@@ -1,9 +1,11 @@
 module Prosumma (
   module Prosumma.Exceptions,
   module Prosumma.Logging,
+  module Prosumma.Textual,
   module Prosumma.Util
 ) where
 
 import Prosumma.Exceptions
 import Prosumma.Logging
+import Prosumma.Textual
 import Prosumma.Util
