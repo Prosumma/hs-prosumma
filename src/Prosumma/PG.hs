a184cd43e7efba6478898a5ef0ad54c162162bc2
Gregory Higley <greg@prosumma.com>
2023-01-16 10:46:57 -0500

    liftPG, sendAWS

---
+++ src/Prosumma/PG.hs
@@ -0,0 +1,17 @@
+module Prosumma.PG (
+  HasConnectionPool(..),
+  liftPG
+) where
+
+import Data.Pool
+import Database.PostgreSQL.Simple (Connection)
+import Piggy
+import RIO
+
+class HasConnectionPool a where
+  getConnectionPool :: a -> Pool Connection
+
+liftPG :: (MonadReader env m, HasConnectionPool env, MonadIO m) => PG a -> m a
+liftPG pg = do
+  pool <- asks getConnectionPool
+  liftIO $ withResource pool $ flip withPG pg
