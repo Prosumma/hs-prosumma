6011b9fb7e87dfd90eef335c6ebd4ca7900b0d4c
Gregory Higley <greg@prosumma.com>
2023-03-26 18:20:44 -0400

    env-extra

--- src/Prosumma.hs
+++ src/Prosumma.hs
@@ -1,10 +1,12 @@
 module Prosumma (
+  module Prosumma.Environment,
   module Prosumma.Exceptions,
   module Prosumma.Textual,
   module Prosumma.Types,
   module Prosumma.Util
 ) where
 
+import Prosumma.Environment
 import Prosumma.Exceptions
 import Prosumma.Textual
 import Prosumma.Types
