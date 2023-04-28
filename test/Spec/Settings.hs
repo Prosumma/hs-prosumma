7b75536c3bd881212cd9ba00c59f98da02a87712
Gregory Higley <greg@prosumma.com>
2023-04-27 20:17:13 -0400

    Settings refinements

--- test/Spec/Settings.hs
+++ test/Spec/Settings.hs
@@ -6,6 +6,7 @@ import Amazonka.DynamoDB
 import Control.Lens ((?~))
 import Data.Generics.Product
 import Prosumma.Settings
+import Prosumma.Util
 import RIO
 import Test.Hspec
 
@@ -14,12 +15,19 @@ import qualified RIO.HashMap as HM
 data Settings = Settings {
   stgsName :: !Text,
   stgsAge  :: !Int,
-  stgsGood :: !Bool
+  stgsGood :: !Bool,
+  stgsWut  :: !(Maybe Int),
+  stgsWho  :: !Text 
 } deriving (Eq, Show)
 
 readPersonSettings :: [HashMap Text AttributeValue] -> Maybe Settings
 readPersonSettings items = readSettings items $ \lookup ->
-  Settings <$> lookup "name" <*> lookup "age" <*> lookup "good"
+  Settings
+    <$> lookup "name"
+    <*> lookup "age"
+    <*> lookup "good"
+    <*> require (lookup "wut")
+    <*> (lookup "who" ?? Just "Greg")
 
 newRow :: Text -> AttributeValue -> HashMap Text AttributeValue
 newRow key value = let attributeKey = newAttributeValue & (field @"s") ?~ key in
@@ -55,7 +63,7 @@ testSettings = do
       let boolAttribute = newAttributeValue & (field @"bool") ?~ False
       let row3 = newRow "good" boolAttribute
       let rows = [row1, row2, row3]
-      let expected = Settings "Vina" 22 False
+      let expected = Settings "Vina" 22 False Nothing "Greg" 
       readPersonSettings rows `shouldBe` Just expected 
     it "fails to initialize a record given invalid data" $ do
       let stringAttribute = newAttributeValue & (field @"s") ?~ "Vina"
