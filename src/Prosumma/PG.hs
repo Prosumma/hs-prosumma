5254feeb9391a8f3c2233c9a0a35b550f0e3f91c
Gregory Higley <greg@prosumma.com>
2023-07-03 02:27:25 -0400

    Haddock

--- src/Prosumma/PG.hs
+++ src/Prosumma/PG.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE FlexibleInstances, GADTs, NamedFieldPuns, RecordWildCards #-}
 
 module Prosumma.PG (
+  close,
+  connectPostgreSQL,
   execute_,
   execute,
   query_,
@@ -14,7 +16,7 @@ module Prosumma.PG (
   PG(..)
 ) where
 
-import Database.PostgreSQL.Simple (formatQuery, Connection, FromRow, ToRow)
+import Database.PostgreSQL.Simple (close, connectPostgreSQL, formatQuery, Connection, FromRow, ToRow)
 import Database.PostgreSQL.Simple.FromField
 import Database.PostgreSQL.Simple.Types
 import Data.List.Safe
@@ -28,7 +30,31 @@ import qualified Database.PostgreSQL.Simple as PG
 
 type ConnectionPool = Pool Connection
 
+-- | A @ConnectionRunner@ is any type which can supply a connection to the @Connection -> IO a@
+-- action passed as the second parameter.
+--
+-- This is designed to be used in the @ReaderT env IO a@ pattern popularized by @RIO@,
+-- in which @env@ implements @ConnectionRunner@. Types exported from this module
+-- which implement @ConnectionRunner@ are @Connection@, @ConnectionPool@ (i.e., 
+-- @Pool Connection@) and @PG@. Implementation for your own types is straightforward.
+-- 
+-- > getUser :: UUID -> RIO (PG Connection) [User]
+-- > getUser id = do
+-- >  env <- ask 
+-- >  runConnection env $ query "SELECT * FROM \"user\" WHERE id = ?" (Only id) 
+-- 
+-- Note that the @query@ function used in the example above is the one from @Database.PostgreSQL.Simple@.
+-- 
+-- However, there's no need to call @runConnection@ directly. Instead, the functions exported from
+-- this module should be used:
+--
+-- > getUser :: UUID -> RIO (PG Connection) [User]
+-- > getUser id = query "SELECT * FROM \"user\" WHERE id = ?" (Only id)   
 class ConnectionRunner r where
+  -- | Runs the @Connection -> IO a@ action.
+  --
+  -- Conforming implementations use @r@ to provide a @Connection@ to the
+  -- @IO@ action. See the instances for @ConnectionPool@ and @Connection@.
   runConnection :: MonadIO m => r -> (Connection -> IO a) -> m a
 
 instance ConnectionRunner ConnectionPool where
@@ -37,6 +63,10 @@ instance ConnectionRunner ConnectionPool where
 instance ConnectionRunner Connection where
   runConnection conn action = liftIO $ action conn
 
+-- | Contains the minimum fields needed to call one of the query functions
+-- exported from this module, such as @execute@, @query@, @value1@, etc.
+--
+-- Of course, @r@ must implement @ConnectionRunner@ for it to work.
 data PG r = PG { pgConnectionRunner :: r, pgLogFunc :: LogFunc }
 
 instance HasLogFunc (PG r) where
@@ -48,6 +78,7 @@ instance ConnectionRunner r => ConnectionRunner (PG r) where
 logSource :: LogSource
 logSource = "SQL"
 
+-- | A GADT whose primary purpose is to hide @q@ from the signature of @run@.
 data SQLQuery where
   SQLQuery :: Query -> SQLQuery
   ParameterizedSQLQuery :: ToRow q => Query -> q -> SQLQuery
@@ -56,19 +87,25 @@ formatSQLQuery :: Connection -> SQLQuery -> IO Text
 formatSQLQuery _ (SQLQuery sql) = return $ toText $ fromQuery sql
 formatSQLQuery conn (ParameterizedSQLQuery sql q) = formatQuery conn sql q <&> toText
 
+-- | Logs a @SQLQuery@ and then runs the @Connection -> IO a@ action with @runConnection@. 
+-- 
+-- This helper function is used by @runSQLQuery@ and @runParameterizedSQLQuery@. 
+--
+-- Note that the log level must be at least DEBUG for the query to be logged.
 run :: (MonadReader env m, ConnectionRunner env, HasLogFunc env, MonadIO m) => SQLQuery -> (Connection -> IO a) -> m a
 run query action = do
   env <- ask
   let logFunc = env^.logFuncL
   runConnection env $ \conn -> do
-    runRIO (Logger logFunc) $ do
-      formattedQuery <- liftIO $ formatSQLQuery conn query
-      logDebugS logSource $ display formattedQuery
+    runRIO (Logger logFunc) $
+      liftIO (formatSQLQuery conn query) >>= logDebugS logSource . display
     liftIO $ action conn 
 
+-- | Runs — but first, logs — an unparameterized SQL query. 
 runSQLQuery :: (MonadReader env m, ConnectionRunner env, HasLogFunc env, MonadIO m) => (Connection -> Query -> IO a) -> Query -> m a
 runSQLQuery action sql = run (SQLQuery sql) $ flip action sql
 
+-- | Runs — but first, logs — a parameterized SQL query.
 runParameterizedSQLQuery :: (MonadReader env m, ConnectionRunner env, HasLogFunc env, MonadIO m, ToRow q) => (Connection -> Query -> q -> IO a) -> Query -> q -> m a
 runParameterizedSQLQuery action sql q = run (ParameterizedSQLQuery sql q) $ slipr action sql q
 
