2294e9bee44ee94a0baf8b6b969f1fc67140885d
Gregory Higley <greg@prosumma.com>
2023-08-26 13:26:45 -0400

    Linting

--- src/Prosumma/AWS.hs
+++ src/Prosumma/AWS.hs
@@ -19,30 +19,14 @@ class HasAWSEnv a where
 instance HasAWSEnv Env where
   getAWSEnv = id
 
-sendAWS :: 
-  (
-    MonadUnliftIO m,
-    AWSRequest r,
-    Typeable r,
-    Typeable (AWSResponse r),
-    MonadReader env m,
-    HasAWSEnv env
-  ) => r -> m (AWSResponse r)
+sendAWS
+  :: (MonadUnliftIO m, AWSRequest r, Typeable r, Typeable (AWSResponse r), MonadReader env m, HasAWSEnv env)
+  => r -> m (AWSResponse r)
 sendAWS r = do
   env <- asks getAWSEnv
   runResourceT $ send env r
 
-sendAWSThrowOnError :: 
-  (
-    MonadUnliftIO m,
-    AWSRequest rq,
-    Typeable rq,
-    MonadThrow m,
-    MonadReader env m,
-    HasAWSEnv env,
-    HasField "httpStatus" rs rs Int Int,
-    Typeable rs,
-    Exception e,
-    rs ~ AWSResponse rq
-  ) => (Int -> e) -> rq -> m rs 
+sendAWSThrowOnError
+  :: (MonadUnliftIO m, AWSRequest rq, Typeable rq, MonadThrow m, MonadReader env m, HasAWSEnv env, HasField "httpStatus" rs rs Int Int, Typeable rs, Exception e, rs ~ AWSResponse rq)
+  => (Int -> e) -> rq -> m rs 
 sendAWSThrowOnError mkException = throwOnHttpStatusError mkException <=< sendAWS
