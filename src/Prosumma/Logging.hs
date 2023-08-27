fa8197b5b2f745a803b6c416829c0e50ba785819
Gregory Higley <greg@prosumma.com>
2023-08-26 23:20:10 -0400

    LogLevel instances

---
+++ src/Prosumma/Logging.hs
@@ -0,0 +1,30 @@
+module Prosumma.Logging (
+  readLogLevel
+) where
+
+import Data.Text.Read
+import Prosumma.Textual
+import RIO hiding (Reader)
+import RIO.Text
+import Text.Printf
+
+instance Textual LogLevel where
+  fromText "Debug" = Just LevelDebug
+  fromText "Info" = Just LevelInfo
+  fromText "Warn" = Just LevelWarn
+  fromText "Error" = Just LevelError
+  fromText text = Just $ LevelOther text
+  toText LevelDebug = "Debug"
+  toText LevelInfo = "Info"
+  toText LevelWarn = "Warn"
+  toText LevelError = "Error"
+  toText (LevelOther text) = text
+
+-- | Doesn't support 'LevelOther'.
+readLogLevel :: Reader LogLevel
+readLogLevel text
+  | Just remainder <- stripPrefix "Debug" text = Right (LevelDebug, remainder)
+  | Just remainder <- stripPrefix "Info" text = Right (LevelInfo, remainder)
+  | Just remainder <- stripPrefix "Warn" text = Right (LevelWarn, remainder)
+  | Just remainder <- stripPrefix "Error" text = Right (LevelError, remainder)
+  | otherwise = Left $ printf "'%s' is not a valid LogLevel." text
