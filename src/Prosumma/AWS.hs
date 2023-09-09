eaa0ffb995983646b1bf4e9eb9cf3dd31ac233bb
Gregory Higley <greg@prosumma.com>
2023-09-09 02:46:29 -0400

    Added AWS type

--- src/Prosumma/AWS.hs
+++ src/Prosumma/AWS.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE DataKinds, FlexibleContexts, FlexibleInstances, TypeFamilies #-}
 
 module Prosumma.AWS (
-  Env,
-  HasAWSEnv(..),
   sendAWS,
-  sendAWSThrowOnError
+  sendAWSThrowOnError,
+  AWS(..),
+  Env,
+  HasAWSEnv(..)
 ) where
 
 import Amazonka
@@ -19,6 +20,17 @@ class HasAWSEnv a where
 instance HasAWSEnv Env where
   getAWSEnv = id
 
+data AWS = AWS {
+  awsEnv :: !Env,
+  awsLogFunc :: !LogFunc
+}
+
+instance HasAWSEnv AWS where
+  getAWSEnv = awsEnv
+
+instance HasLogFunc AWS where
+  logFuncL = lens awsLogFunc $ \aws awsLogFunc -> aws{awsLogFunc}
+
 sendAWS
   :: (MonadUnliftIO m, AWSRequest r, Typeable r, Typeable (AWSResponse r), MonadReader env m, HasAWSEnv env)
   => r -> m (AWSResponse r)
@@ -30,3 +42,4 @@ sendAWSThrowOnError
   :: (MonadUnliftIO m, AWSRequest rq, Typeable rq, MonadThrow m, MonadReader env m, HasAWSEnv env, HasField "httpStatus" rs rs Int Int, Typeable rs, Exception e, rs ~ AWSResponse rq)
   => (Int -> e) -> rq -> m rs 
 sendAWSThrowOnError mkException = throwOnHttpStatusError mkException <=< sendAWS
+
