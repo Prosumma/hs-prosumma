27d77c38ca84418c17846b1c4fa1b4dca2d60416
Gregory Higley <greg@prosumma.com>
2024-04-04 16:25:35 -0400

    Cache rewrite

--- src/Prosumma/Cache.hs
+++ src/Prosumma/Cache.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DeriveAnyClass, DeriveGeneric, OverloadedRecordDot #-}
 
 -- | Provides a thread-safe in-memory cache.
 --
@@ -7,240 +7,198 @@
 -- put "cache" at the beginning, e.g., @cacheGet@, @cachePut@.
 --
 -- Order of arguments closely follows those used for @Map@.
+-- 
+-- Reaping is the process of 
 module Prosumma.Cache (
+  Cache,
+  Entry(..),
+  Fetch,
+  Outcome(..),
+  Reap,
+  Result,
+  Store,
   cacheDelete,
-  cacheDeletes,
   cacheGet,
-  cacheGetAll,
-  cacheGetAllResults,
-  cacheGetFetchResult,
-  cacheGetKeys,
   cacheGetResult,
   cacheGetResults,
   cacheGets,
-  cacheInsert,
-  cacheInserts,
   cachePut,
   cachePuts,
-  clearCache,
   createCache,
-  hour,
-  minute,
   newCache,
-  resultToFetchResult,
-  resultToMaybe,
-  Prosumma.Cache.second,
-  setCache,
+  reapAlways,
   sizeCache,
-  Cache,
-  Result(..),
-  Sentinel(..)
+  withSizeLimitBy,
+  withTTLBy
 ) where
 
-import Data.Either.Extra
 import Data.Hashable
+import Prosumma.Util
 import RIO
+import RIO.List (sortOn)
 import RIO.Time
 
-import qualified RIO.HashMap as HM
+import qualified RIO.HashMap as HashMap
 
--- | When an entry is fetched, it can succeed or fail.
--- @FetchResult@ tells us which of these it was.
-type FetchResult v = Either SomeException v
+distantFuture :: UTCTime
+distantFuture = UTCTime (fromGregorian 9999 12 31) (secondsToDiffTime 86399)
+
 type Fetch k v = k -> IO v
-type TTL = NominalDiffTime
-
-second :: Int
-second = 1
-
-minute :: Int
-minute = Prosumma.Cache.second * 60
-
-hour :: Int
-hour = minute * 60
 
 data Entry v = Entry {
-  entryWhen :: !UTCTime,
-  entryValue :: !v
-}
+  added    :: !UTCTime,
+  accessed :: !UTCTime,
+  value    :: !v
+} deriving (Eq, Ord, Show)
 
 type Store k v = HashMap k (Entry v)
 
+type Reap k v = Store k v -> IO (Store k v)
+
+withSizeLimitBy :: Hashable k => (Entry v -> UTCTime) -> Int -> Reap k v
+withSizeLimitBy attr limit store
+  | HashMap.size store <= limit = return store
+  | otherwise = return $ HashMap.fromList $ take limit $ sortOn (Down . attr . snd) $ HashMap.toList store
+
+withTTLBy :: Hashable k => (Entry v -> UTCTime) -> NominalDiffTime -> Reap k v
+withTTLBy attr ttl store = do
+  now <- getCurrentTime
+  return $ foldl' (handle now) store (HashMap.keys store)
+  where
+    alter _ Nothing = Nothing
+    alter now (Just entry) = if diffUTCTime now (attr entry) <= ttl 
+      then Just entry
+      else Nothing
+    handle now store k = HashMap.alter (alter now) k store 
+
+reapAlways :: Reap k v
+reapAlways _ = return HashMap.empty 
+
 data Cache k v = Cache {
-  cacheStore :: !(MVar (Store k v)),
-  cacheTTL   :: !(Maybe TTL),
-  cacheFetch :: !(Fetch k v)
+  reap  :: !(Reap k v),
+  fetch :: !(Fetch k v),
+  store :: !(MVar (Store k v))
 }
 
--- | @Result@ tells us precisely what happened when attempting to retrieve
--- an entry from the cache.
---
--- The second parameter of @Fetched@ indicates whether the entry was stale or not.
---
--- Note that the @Eq@ instance for @Result@ is defective in the case where @FetchResult@
--- is @Left@. It does not compare exceptions for equality, treating them all as equal.
--- The @Eq@ instance is used for unit tests and is not much use for anything else.
-data Result v = Cached !v | Fetched !Bool !(FetchResult v) deriving Show
+data Outcome = Cached | Fetched deriving (Eq, Ord, Show)
+type Result v = Either SomeException (v, Outcome)
 
-instance Eq v => Eq (Result v) where
-  (Cached v1) == (Cached v2) = v1 == v2
-  (Fetched isStale1 (Right v1)) == (Fetched isStale2 (Right v2)) = isStale1 == isStale2 && v1 == v2
-  (Fetched isStale1 (Left _)) == (Fetched isStale2 (Left _)) = isStale1 == isStale2
-  _ == _ = False
-
-data NoException = NoException deriving (Show, Typeable)
-instance Exception NoException
-
--- | A useful key when the `Cache` will only ever contain one element.
-data Sentinel = Sentinel deriving (Eq, Ord, Show)
-
-instance Hashable Sentinel where
-  hashWithSalt salt _ = hashWithSalt salt (2305843009213693951 :: Int)
+data Sentinel = Sentinel deriving (Eq, Ord, Show, Generic, Typeable, Hashable)
 
 withLockedStore :: MonadIO m => Cache k v -> (Store k v -> m (a, Store k v)) -> m a
-withLockedStore Cache{..} useStore = do
-  store <- takeMVar cacheStore
+withLockedStore cache useStore = do
+  store <- takeMVar cache.store
   (result, resultStore) <- useStore store
-  putMVar cacheStore resultStore 
+  putMVar cache.store resultStore
   return result
 
-withLockedStore_ :: MonadIO m => (Store k v -> m (Store k v)) -> Cache k v -> m ()
-withLockedStore_ useStore Cache{..} = takeMVar cacheStore >>= useStore >>= putMVar cacheStore
+withLockedStore_ :: MonadIO m => Cache k v -> (Store k v -> m (Store k v)) -> m ()
+withLockedStore_ cache useStore = takeMVar cache.store >>= useStore >>= putMVar cache.store
 
-ofLockedStore :: MonadIO m => (Store k v -> a) -> Cache k v -> m a
-ofLockedStore useStore Cache{..} = do
-  store <- takeMVar cacheStore
-  let result = useStore store
-  putMVar cacheStore store
-  return result
+withReapedStore
+  :: (MonadUnliftIO m)
+  => Reap k v -> (Store k v -> m (a, Store k v)) -> Store k v -> m (a, Store k v)
+withReapedStore reap run store = withRunInIO $ \runInIO -> liftIO (reap store) >>= runInIO . run
 
-noException :: SomeException
-noException = toException NoException
+withReapedStore_
+  :: (MonadUnliftIO m)
+  => Reap k v -> (Store k v -> m (Store k v)) -> Store k v -> m (Store k v)
+withReapedStore_ reap run store = withRunInIO $ \runInIO -> liftIO (reap store) >>= runInIO . run 
 
-resultToMaybe :: Result v -> Maybe v
-resultToMaybe (Cached v) = Just v
-resultToMaybe (Fetched _ (Right v)) = Just v
-resultToMaybe _ = Nothing
+storeGetResult'
+  :: (Hashable k, MonadUnliftIO m)
+  => k -> Fetch k v -> Store k v -> m (Result v, Store k v)
+storeGetResult' key fetch store = do
+  now <- getCurrentTime
+  case HashMap.lookup key store of
+    Just entry -> do
+      let resultEntry = entry{accessed=now}
+      let resultStore = HashMap.update (const $ Just resultEntry) key store
+      return (Right (resultEntry.value, Cached), resultStore)
+    Nothing -> do
+      fetchResult <- liftIO $ catch (Right <$> fetch key) (return . Left)
+      case fetchResult of
+        Left e -> return (Left e, store)
+        Right value -> do
+          let entry = Entry now distantFuture value
+          let resultStore = HashMap.insert key entry store
+          return (Right (value, Fetched), resultStore)
 
-resultToFetchResult :: Result v -> FetchResult v
-resultToFetchResult (Cached v) = Right v
-resultToFetchResult (Fetched _ (Right v)) = Right v
-resultToFetchResult (Fetched _ (Left e)) = Left e
-
-maybeToFetchResult :: Maybe v -> FetchResult v
-maybeToFetchResult (Just v) = Right v
-maybeToFetchResult Nothing = Left noException
-
-put :: Hashable k => UTCTime -> k -> FetchResult v -> Store k v -> Store k v
-put _ key (Left _) store = HM.delete key store
-put now key (Right value) store = HM.insert key (Entry now value) store
-
-storePut :: (Hashable k, MonadIO m) => k -> FetchResult v -> Store k v -> m (Store k v)
-storePut key value store = do
-  now <- liftIO getCurrentTime
-  return $ put now key value store
-
-storePuts :: (Hashable k, MonadIO m) => HashMap k (FetchResult v) -> Store k v -> m (Store k v)
-storePuts newEntries store = do
-  now <- liftIO getCurrentTime
-  return $ HM.foldrWithKey (put now) store newEntries
-
--- | See the 'cacheGetResult' function for a discussion.
 storeGetResult
   :: (Hashable k, MonadUnliftIO m)
-  => k -> Maybe TTL -> Fetch k v -> Store k v -> m (Result v, Store k v)
-storeGetResult key ttl fetch store = case (HM.lookup key store, ttl) of
-  (Just entry, Just ttl) -> handleStale entry ttl store -- Check staleness
-  (Just Entry{..}, Nothing) -> return (Cached entryValue, store)  -- Entry was found, no TTL specified
-  _ -> getFetchResult False store -- Entry must be fetched
-  where
-    handleStale Entry{..} ttl store = do
-      now <- liftIO getCurrentTime
-      if diffUTCTime now entryWhen <= ttl
-        then return (Cached entryValue, store) -- Entry was found and is not stale
-        else getFetchResult True store -- Entry was found but is stale
-    getFetchResult isStale store = do
-      fetchResult <- liftIO $ catchAny (Right <$> fetch key) (return . Left)
-      newStore <- storePut key fetchResult store
-      return (Fetched isStale fetchResult, newStore)
+  => k -> Reap k v -> Fetch k v -> Store k v -> m (Result v, Store k v)
+storeGetResult key reap fetch = withReapedStore reap $ storeGetResult' key fetch 
 
 storeGetResults
-  :: (Hashable k, MonadUnliftIO m, Foldable f)
-  => f k -> Maybe TTL -> Fetch k v -> Store k v -> m (HashMap k (Result v), Store k v)
-storeGetResults keys ttl fetch store = foldM get (mempty, store) keys
+  :: (Hashable k, MonadUnliftIO m)
+  => Set k -> Reap k v -> Fetch k v -> Store k v -> m (HashMap k (Result v), Store k v)
+storeGetResults keys reap fetch = withReapedStore reap $ \store ->
+  foldM get (mempty, store) keys
   where
-    get (results, store) key = do
-      (result, nextStore) <- storeGetResult key ttl fetch store
-      return (HM.insert key result results, nextStore)
+    get (results, store) key = do 
+      (result, nextStore) <- storeGetResult' key fetch store
+      return (HashMap.insert key result results, nextStore)
+
+storePut'
+  :: (MonadUnliftIO m, Hashable k)
+  => k -> Maybe v -> Store k v -> m (Store k v)
+storePut' key Nothing store = return $ HashMap.delete key store
+storePut' key (Just value) store = do
+  now <- getCurrentTime
+  return $ HashMap.alter (alterEntry now) key store
+  where
+    alterEntry now Nothing = Just $ Entry now distantFuture value
+    alterEntry now (Just oldEntry) = Just $ oldEntry{accessed=now,value}
+
+storePut
+  :: (MonadUnliftIO m, Hashable k)
+  => Reap k v -> k -> Maybe v -> Store k v -> m (Store k v)
+storePut reap key newValue = withReapedStore_ reap $ storePut' key newValue 
+
+storePuts
+  :: (MonadUnliftIO m, Hashable k)
+  => Reap k v -> HashMap k (Maybe v) -> Store k v -> m (Store k v)
+storePuts reap newValues = withReapedStore_ reap $ \store ->
+  foldM put store (HashMap.toList newValues)
+  where
+    put store (key, newValue) = storePut' key newValue store 
+
+storeDelete :: (MonadUnliftIO m, Hashable k) => Reap k v -> k -> Store k v -> m (Store k v)
+storeDelete reap key = withReapedStore_ reap $ return . HashMap.delete key 
 
 newStore :: MonadIO m => HashMap k v -> m (Store k v)
-newStore store = liftIO getCurrentTime >>= \now -> return $ HM.map (Entry now) store
+newStore store = liftIO getCurrentTime >>= \now -> return $ HashMap.map (Entry now distantFuture) store
 
-cachePut :: (Hashable k, MonadIO m) => k -> Maybe v -> Cache k v -> m ()
-cachePut key maybeValue = withLockedStore_ $ storePut key (maybeToFetchResult maybeValue)
-
-cachePuts :: (Hashable k, MonadIO m) => HashMap k (Maybe v) -> Cache k v -> m ()
-cachePuts newEntries = withLockedStore_ $ storePuts (HM.map maybeToFetchResult newEntries)
-
-cacheDelete :: (Hashable k, MonadIO m) => k -> Cache k v -> m ()
-cacheDelete key = cachePut key Nothing
-
-cacheDeletes :: (Hashable k, MonadIO m, Foldable f) => f k -> Cache k v -> m ()
-cacheDeletes = cachePuts . foldr (`HM.insert` Nothing) mempty
-
-cacheInsert :: (Hashable k, MonadIO m) => k -> v -> Cache k v -> m ()
-cacheInsert key = cachePut key . Just
-
-cacheInserts :: (Hashable k, MonadIO m) => HashMap k v -> Cache k v -> m ()
-cacheInserts newEntries = cachePuts $ HM.map Just newEntries
-
--- | Attempts to get an entry and reports the result in the 'Result' type.
--- This function is thread-safe.
---
--- In most cases, callers will not care what the @Result@ is. They just want the
--- value. In that case, use 'cacheGet'. 
 cacheGetResult :: (Hashable k, MonadUnliftIO m) => k -> Cache k v -> m (Result v)
-cacheGetResult key cache@Cache{..} = withLockedStore cache $ \store -> storeGetResult key cacheTTL cacheFetch store
+cacheGetResult key cache = withLockedStore cache $ storeGetResult key cache.reap cache.fetch
 
-cacheGetFetchResult :: (Hashable k, MonadUnliftIO m) => k -> Cache k v -> m (FetchResult v)
-cacheGetFetchResult key cache = resultToFetchResult <$> cacheGetResult key cache
+cacheGetResults :: (Hashable k, MonadUnliftIO m) => Set k -> Cache k v -> m (HashMap k (Result v))
+cacheGetResults keys cache = withLockedStore cache $ storeGetResults keys cache.reap cache.fetch
 
--- | Same as 'cacheGetResult' but gets more than one key.
---
--- When attempting to get multiple keys, this method is more efficient than calling @cacheGetResult@ multiple times.
-cacheGetResults :: (Hashable k, MonadUnliftIO m, Foldable f) => f k -> Cache k v -> m (HashMap k (Result v))
-cacheGetResults keys cache@Cache{..} = withLockedStore cache $ \store -> storeGetResults keys cacheTTL cacheFetch store 
-
-cacheGetAllResults :: (Hashable k, MonadUnliftIO m) => Cache k v -> m (HashMap k (Result v))
-cacheGetAllResults cache@Cache{..} = withLockedStore cache $ \store -> storeGetResults (HM.keys store) cacheTTL cacheFetch store
-
-cacheGetAll :: (Hashable k, MonadUnliftIO m) => Cache k v -> m (HashMap k (Maybe v)) 
-cacheGetAll cache = HM.map resultToMaybe <$> cacheGetAllResults cache
-
-cacheGetKeys :: MonadIO m => Cache k v -> m [k]
-cacheGetKeys = ofLockedStore HM.keys 
-
--- | Simplified version of @cacheGetResult@.
 cacheGet :: (Hashable k, MonadUnliftIO m) => k -> Cache k v -> m (Maybe v)
-cacheGet key cache = resultToMaybe <$> cacheGetResult key cache
+cacheGet key cache = fmap fst . hush <$> cacheGetResult key cache
 
-cacheGets :: (Hashable k, MonadUnliftIO m, Foldable f) => f k -> Cache k v -> m (HashMap k (Maybe v))
-cacheGets keys cache = HM.map resultToMaybe <$> cacheGetResults keys cache
+cacheGets :: (Hashable k, MonadUnliftIO m) => Set k -> Cache k v -> m [v]
+cacheGets keys cache = mapMaybe (fmap fst . hush) . HashMap.elems <$> cacheGetResults keys cache
 
-createCache :: (Hashable k, MonadIO m) => Maybe Int -> Fetch k v -> m (Cache k v)
-createCache ttl cacheFetch = newCache ttl cacheFetch mempty
+cachePut 
+  :: (MonadUnliftIO m, Hashable k)
+  => k -> Maybe v -> Cache k v -> m ()
+cachePut key value cache = withLockedStore_ cache $ storePut cache.reap key value
 
-setCache :: MonadIO m => HashMap k v -> Cache k v -> m ()
-setCache store Cache{..} = newStore store >>= void . swapMVar cacheStore
+cachePuts
+  :: (MonadUnliftIO m, Hashable k)
+  => HashMap k (Maybe v) -> Cache k v -> m ()
+cachePuts newValues cache = withLockedStore_ cache $ storePuts cache.reap newValues
 
-newCache :: MonadIO m => Maybe Int -> Fetch k v -> HashMap k v -> m (Cache k v)
-newCache ttl cacheFetch store = do
-  let cacheTTL = fromIntegral <$> ttl
-  cacheStore <- newStore store >>= newMVar
-  return Cache{..}
+cacheDelete :: (MonadUnliftIO m, Hashable k) => k -> Cache k v -> m ()
+cacheDelete key cache = withLockedStore_ cache $ storeDelete cache.reap key
 
-clearCache :: (Hashable k, MonadIO m) => Cache k v -> m ()
-clearCache = setCache mempty
+newCache :: MonadIO m => Reap k v -> Fetch k v -> HashMap k v -> m (Cache k v)
+newCache reap fetch store = Cache reap fetch <$> (newStore store >>= newMVar)
 
-sizeCache :: MonadIO m => Cache k v -> m Int
-sizeCache = ofLockedStore HM.size 
+createCache :: (MonadIO m, Hashable k) => Reap k v -> Fetch k v -> m (Cache k v)
+createCache reap fetch = newCache reap fetch mempty
+
+sizeCache :: MonadUnliftIO m => Cache k v -> m Int
+sizeCache cache = withLockedStore cache $ \store -> return (HashMap.size store, store)
--- test/Spec/Cache.hs
+++ test/Spec/Cache.hs
@@ -1,129 +1,97 @@
+{-# LANGUAGE ExistentialQuantification, ScopedTypeVariables #-}
+
 module Spec.Cache (testCache) where
 
 import Prosumma.Cache
 import RIO
-import RIO.HashMap.Partial ((!))
+import RIO.List (sort)
 import System.Random
 import Test.Hspec
 
-import qualified RIO.HashMap as HM
+import qualified RIO.HashMap as HashMap
+import qualified RIO.Set as Set
 
-newtype MissingValueException = MissingValueException Text deriving (Show, Typeable)
+data MissingValueException = MissingValueException deriving Show
 instance Exception MissingValueException
 
-get :: Text -> IO (Maybe Int)  
-get "one" = return $ Just 1 
-get "random" = Just <$> randomRIO (1, 100000)
-get "exception" = error "exception"
-get _ = return Nothing 
+missingValueException :: Proxy MissingValueException
+missingValueException = Proxy
 
-isFetched :: Eq v => Bool -> v -> Result v -> Bool
-isFetched staleExpected a (Fetched staleActual (Right b)) = staleExpected == staleActual && a == b 
-isFetched _ _ (Fetched _ (Left _)) = False 
-isFetched _ _ (Cached _) = False
+isException :: forall e. Exception e => Proxy e -> SomeException -> Bool
+isException _ ex = isJust (fromException ex :: Maybe e)
 
-isFetchError :: Result v -> Bool
-isFetchError (Fetched _ (Left _)) = True 
-isFetchError _ = False
+get :: Text -> IO Int
+get "one" = return 1
+get "two" = return 2
+get "three" = return 3
+get "four" = return 4
+get "random" = randomRIO (1, 100000)
+get _ = throwIO MissingValueException
+
+resultShouldBe :: (Eq v, Show v) => Result v -> (v, Outcome) -> Expectation
+resultShouldBe actual expected = actual `shouldSatisfy` either (const False) (== expected)
+
+resultShouldThrow :: (Show v, Exception e) => Result v -> Proxy e -> Expectation
+resultShouldThrow actual proxy = actual `shouldSatisfy` either (isException proxy) (const False) 
 
 testCache :: Spec
-testCache = do 
+testCache = do
   describe "cacheGetResult" $ do
     it "fetches an existing value from the cache" $ do
-      cache <- newCache Nothing get $ HM.singleton "random" (Just 2)
-      result <- cacheGetResult "random" cache
-      result `shouldBe` Cached (Just 2) 
-    it "refetches a stale value from the cache" $ do
-      cache <- newCache (Just 0) get $ HM.singleton "random" (Just 0)
-      result <- cacheGetResult "random" cache
-      let value = join $ resultToMaybe result
-      result `shouldSatisfy` isFetched True value 
-    it "fetches an absent value from the cache" $ do
-      cache <- createCache Nothing get
+      cache <- newCache return get (HashMap.singleton "one" 1) 
       result <- cacheGetResult "one" cache
-      result `shouldSatisfy` isFetched False (Just 1)
-    it "fetches an unexpired value from the cache" $ do
-      cache <- newCache (Just 5) get $ HM.singleton "one" (Just 1)
-      result <- cacheGetResult "one" cache
-      result `shouldBe` Cached (Just 1)
+      result `resultShouldBe` (1, Cached)
+    it "fetches a value when it is not present in the cache" $ do
+      cache <- createCache return get
+      result <- cacheGetResult "two" cache
+      result `resultShouldBe` (2, Fetched)
     it "does not cache if an exception occurs" $ do
-      cache <- createCache Nothing get
-      result <- cacheGetResult "exception" cache
-      result `shouldSatisfy` isFetchError
+      cache <- createCache return get
+      result <- cacheGetResult "seventy" cache
+      result `resultShouldThrow` missingValueException
     it "works across threads" $ do
-      cache <- createCache Nothing get
-      let getRandom = join <$> cacheGet "random" cache
+      cache <- createCache return get
+      let getRandom = cacheGet "random" cache
       let threadCount = [1..100] :: [Int]
       values <- forConcurrently threadCount $ const getRandom
       values `shouldSatisfy` allEqual
-  describe "cacheGetAll" $ do
-    it "gets all entries" $ do
-      let entries = HM.fromList [("foo", Just 999), ("bar", Just 666)]
-      cache <- newCache Nothing get entries 
-      results <- HM.map join <$> cacheGetAll cache
-      results `shouldBe` entries 
   describe "cacheGets" $ do
-    it "gets many results as a HashMap" $ do
-      cache <- newCache Nothing get $ HM.singleton "two" (Just 2)
-      results <- HM.map join <$> cacheGets ["one", "two"] cache
-      results `shouldBe` HM.fromList [("one", Just 1), ("two", Just 2)]
-    it "handles any Foldable" $ do
-      cache <- newCache Nothing get $ HM.singleton "two" (Just 2)
-      results <- cacheGets Nothing cache
-      results `shouldBe` mempty
+    it "gets multiple items from the cache, skipping entries which fail" $ do
+      cache <- createCache return get
+      results <- cacheGets (Set.fromList ["one", "two", "eleven"]) cache 
+      sort results `shouldBe` [1, 2]
+  describe "withTTLBy" $ do
+    it "reaps an item if it has expired" $ do
+      cache <- createCache (withTTLBy accessed 0.1) get
+      result1 <- cacheGetResult "one" cache
+      result1 `resultShouldBe` (1, Fetched)
+      result2 <- cacheGetResult "one" cache
+      result2 `resultShouldBe` (1, Cached)
+      threadDelay 150000
+      result3 <- cacheGetResult "one" cache
+      result3 `resultShouldBe` (1, Fetched)
   describe "cachePut" $ do
-    it "puts a value into the cache" $ do
-      cache <- createCache Nothing get
-      cachePut "random" (Just (Just 0)) cache
-      value <- join <$> cacheGet "random" cache
-      value `shouldBe` Just 0 
-  describe "cachePuts" $ do
-    it "puts multiple values into the cache" $ do
-      cache <- newCache Nothing get $ HM.fromList [("one", Just 5), ("random", Just 0)]
-      let newEntries = HM.fromList [("one", Nothing), ("random", Nothing), ("x", Just (Just 9))]
-      cachePuts newEntries cache
-      values <- HM.map join <$> cacheGets ["one", "random", "x"] cache
-      values ! "random" `shouldNotBe` Just 0
-      values ! "one" `shouldBe` Just 1
-      values ! "x" `shouldBe` Just 9
+    it "puts an item manually into the cache" $ do
+      cache <- createCache return get
+      cachePut "one" (Just 1) cache
+      result <- cacheGetResult "one" cache
+      result `resultShouldBe` (1, Cached)
+  describe "withSizeLimitBy" $ do
+    it "reaps items if the cache grows above a certain size" $ do
+      cache <- createCache (withSizeLimitBy accessed 2) get 
+      void $ cacheGet "one" cache
+      void $ cacheGet "two" cache
+      void $ cacheGet "three" cache
+      result <- cacheGetResult "one" cache
+      result `resultShouldBe` (1, Fetched)
   describe "cacheDelete" $ do
-    it "removes a value from the cache" $ do
-      cache <- newCache Nothing get $ HM.singleton "ninety" (Just 90)
-      cacheDelete "ninety" cache
-      value <- join <$> cacheGet "ninety" cache
-      value `shouldBe` Nothing
-  describe "cacheDeletes" $ do
-    it "removes multiple values from the cache" $ do
-      cache <- newCache Nothing get $ HM.fromList [("foo", Just 999), ("bar", Just 666)]
-      cacheDeletes ["foo", "bar"] cache
-      size <- sizeCache cache
-      size `shouldBe` 0
-  describe "cacheInsert" $ do
-    it "inserts a value into the cache" $ do
-      cache <- createCache Nothing get
-      cacheInsert "foo" (Just 999) cache
-      foo <- join <$> cacheGet "foo" cache
-      foo `shouldBe` Just 999
-  describe "cacheInserts" $ do
-    it "inserts multiple values into the cache" $ do
-      let newValues = HM.fromList [("foo", Just 999), ("bar", Just 666)]
-      cache <- createCache Nothing get
-      cacheInserts newValues cache
-      values <- HM.map join <$> cacheGets ["foo", "bar"] cache
-      values `shouldBe` newValues
-  describe "setCache" $ do
-    it "replaces the cache with new values" $ do
-      cache <- createCache Nothing get
-      let newStore = HM.singleton "twelve" (Just 12)
-      setCache newStore cache
-      value <- join <$> cacheGet "twelve" cache
-      value `shouldBe` Just 12
-  describe "clearCache" $ do
-    it "clears the cache" $ do
-      cache <- newCache Nothing get $ HM.singleton "random" (Just 0)
-      clearCache cache
-      value <- join <$> cacheGet "random" cache
-      value `shouldNotBe` Just 0
+    it "removes an item from the cache so it can be refetched" $ do
+      cache <- newCache return get $ HashMap.singleton "one" 1
+      result1 <- cacheGetResult "one" cache
+      result1 `resultShouldBe` (1, Cached)
+      cacheDelete "one" cache
+      result2 <- cacheGetResult "one" cache
+      result2 `resultShouldBe` (1, Fetched)
 
 allEqual :: Eq a => [a] -> Bool
 allEqual [] = True
