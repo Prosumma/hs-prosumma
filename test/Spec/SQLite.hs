372a5a7c9552316503816fe655735849703b50df
Gregory Higley <greg@prosumma.com>
2024-09-03 11:50:51 -0400

    SQLite

--- test/Spec/SQLite.hs
+++ test/Spec/SQLite.hs
@@ -1,9 +1,27 @@
-module Spec.SQLite where
+{-# LANGUAGE DeriveGeneric #-}
 
+module Spec.SQLite (testSQLite) where
+
+import Database.SQLite.Simple (FromRow, ToRow)
+import Prosumma.SQLite
 import RIO
 import Test.Hspec
 
+data User = User {
+  id :: !Int,
+  name :: !Text
+} deriving (Eq, Show, Generic)
+
+instance FromRow User where
+instance ToRow User where
+
 testSQLite :: Spec
-testSQLite = describe "foo" $ do
-  it "bars" $ do
-    True `shouldBe` True
+testSQLite = describe "SQLite integration" $ do
+  it "works" $ do
+    conn <- liftIO $ open ":memory:"
+    let input = User 3 "Bob"
+    output <- runRIO conn $ do
+      execute_ "CREATE TABLE IF NOT EXISTS user(id INT NOT NULL PRIMARY KEY, name TEXT NOT NULL)"
+      execute "INSERT INTO user(id, name) SELECT ?, ?" input 
+      query1_ "SELECT id, name FROM user LIMIT 1"
+    input `shouldBe` output
