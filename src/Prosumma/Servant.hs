952c279f1eeca40ea6883f440313b896fcfc1b6e
Gregory Higley <greg@prosumma.com>
2024-12-23 01:31:49 -0500

    No State Transform
    - Got rid of unnecessary transform parameter in runApplication
    - 1.8.0.0

--- src/Prosumma/Servant.hs
+++ src/Prosumma/Servant.hs
@@ -9,8 +9,7 @@ module Prosumma.Servant (
   runApplicationWithContext,
   ServerExceptionHandler,
   ServerHandler,
-  ServerResponse,
-  StateTransform,
+  ServerResponse
 ) where
 
 import Control.Monad.Error.Class
@@ -22,7 +21,6 @@ import Servant
 type ServerResponse a = Either ServerError a
 type ServerHandler a = Handler (ServerResponse a)
 type ServerExceptionHandler s a = SomeException -> RIO s (ServerResponse a)
-type StateTransform s a = RIO s a -> RIO s a
 
 mapServerException :: SomeException -> ServerResponse a 
 mapServerException e = case fromException e :: Maybe ServerError of 
@@ -37,22 +35,22 @@ loggingExceptionHandler e = do
   logError $ displayShow e
   defaultExceptionHandler e
 
-runApp :: ServerExceptionHandler s a -> StateTransform s a -> s -> RIO s a -> ServerHandler a
-runApp handler transform state app = liftIO $ runRIO state $ catch (Right <$> transform app) handler
+runApp :: ServerExceptionHandler s a -> s -> RIO s a -> ServerHandler a
+runApp handler state app = liftIO $ runRIO state $ catch (Right <$> app) handler
 
-mapApp :: ServerExceptionHandler s a -> StateTransform s a -> s -> RIO s a -> Handler a
-mapApp handler transform state app = runApp handler transform state app >>= liftEither 
+mapApp :: ServerExceptionHandler s a -> s -> RIO s a -> Handler a
+mapApp handler state app = runApp handler state app >>= liftEither 
 
 runApplication
   :: forall (api :: Type) s. HasServer api '[]
-  => (forall a. ServerExceptionHandler s a) -> (forall a. StateTransform s a) -> Proxy api -> ServerT api (RIO s) -> s -> Application
-runApplication handler transform proxy api state = serve proxy $ hoistServer proxy (mapApp handler transform state) api
+  => (forall a. ServerExceptionHandler s a) -> Proxy api -> ServerT api (RIO s) -> s -> Application
+runApplication handler proxy api state = serve proxy $ hoistServer proxy (mapApp handler state) api
 
 runApplicationWithContext
   :: forall (api :: Type) (context :: [Type]) s. (HasServer api context, HasContextEntry (context .++ DefaultErrorFormatters) ErrorFormatters)
-  => Context context -> (forall a. ServerExceptionHandler s a) -> (forall a. StateTransform s a) -> Proxy api -> ServerT api (RIO s) -> s -> Application
-runApplicationWithContext context handler transform proxy api state =
-  serveWithContext proxy context $ hoistServerWithContext proxy (Proxy :: Proxy context) (mapApp handler transform state) api
+  => Context context -> (forall a. ServerExceptionHandler s a) -> Proxy api -> ServerT api (RIO s) -> s -> Application
+runApplicationWithContext context handler proxy api state =
+  serveWithContext proxy context $ hoistServerWithContext proxy (Proxy :: Proxy context) (mapApp handler state) api
 
 maybeThrow404 :: MonadIO m => m (Maybe a) -> m a
 maybeThrow404 action = action >>= flip whenNothing (throwIO err404)
